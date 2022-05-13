e1b87b7360451df5ab7dddc91a0dcc259e52f207
jcmvdb <mvdb.jeroen@gmail.com>
2022-05-13 20:05:16 +0200

    command handler

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
-const {Client, Intents} = require("discord.js");
+const {Client, Intents, Collection} = require("discord.js");
 const botConfig = require('./botConfig.json');
-const secrets = require('./secrets.json')
+const secrets = require('./secrets.json');
+const fs = require("fs");
 
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.help.name, command);
+    console.log(`The file ${command.help.name}.js is loaded`);
+}
+
 client.once("ready", () => {
     console.log(`${client.user.username} is ready`);
-    client.user.setActivity("a programming game", {type: "PLAYING"})
+    client.user.setActivity("a programming game", {type: "PLAYING"});
 });
 
-client.on("messageCreate", message => {
+client.on("messageCreate", async message => {
     if (message.author.bot) return;
 
     var prefix = botConfig.prefix;
     var messageArray = message.content.split(" ");
     var command = messageArray[0];
 
-    if(command === `${prefix}hallo`) {
-        return message.channel.send(`hallo ${message.author.username}`)
-    }
-    if (command === `${prefix}doei`) {
-        return message.channel.send(`doei ${message.author.username}`)
+    if (!message.content.startsWith(prefix)) return;
+
+    const commandData = client.commands.get(command.slice(prefix.length));
+
+    if (!commandData) return;
+
+    var arguments = messageArray.slice(1);
+
+    try {
+        await commandData.run(client, message, arguments);
+        console.log(`${client.user.username} has handeld a command`)
+    } catch (error) {
+        console.log(error)
+        await message.reply("Something went wrong with the command, please contact a moderator or admin");
     }
 });
 
